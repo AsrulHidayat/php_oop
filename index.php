c67728b95c859d3d4442823f135bdd1cd6d06c53
Asrul Hidayat <136725199+AsrulHidayat@users.noreply.github.com>
2024-02-10 11:16:44 +0800

    Study : CONSTRUCTOR & DESTRUCTOR (MATERI 2)

--- index.php
+++ index.php
@@ -1,33 +1,55 @@
 <?php
-//CLASS & OBJECT (MATERI 1)
-    // memanggil class
+    // CONSTRUCTOR & DESTRUCTOR (MATERI 2)
     require "Model/Robot.php";
-    // membuat objek
-    $robot1 = new Robot();
-        // ini adalah cara yang baik untuk mengisi object/nilai dalam property
 
-        // cara 1
-        // $robot1->name = "Optimus prime";
-        // $robot1->color = "blue-white-red";
-        // $robot1->size = "large";
-
-        // cara 2
-        $robot1->set_name("Optimus prime");
-        $robot1->set_color("blue-white-red");
-        $robot1->set_size("large");
-
-    $robot2 = new Robot();
-        // cara 1
-        // $robot2->name = "Bumblebee";
-        // $robot2->color = "yellow";
-        // $robot2->size = "small";
-        // cara 2
-        $robot2->set_name("Bumblebee");
-        $robot2->set_color("yellow");
-        $robot2->set_size("small");
-
-    // melihat objek di halaman
+    // Constructor : Dijalankan ketika objek dibuat
+    // Perhatikan dengan materi 1, jauh berbeda dalam pemanggilannya. Yang mana ini lebih singkat
+    $robot1 = new Robot("Optimus Prime","blue-white-red", "large");
     var_dump($robot1);
-    echo"<br>";
-    var_dump($robot2);
+
+    echo "<br><br>";
+    // unset : dihancurkan. 
+    // ketika memakai unset program akan  terpotong di bagian atas atau menghancurkan objek diatas sehingga destruct akan berjalan kemudian dilanjutkan dengan kode program di bawah penempatana unset.
+    unset($robot1);
+
+    echo "<br><br>";
+    echo "script sudah selesai";
+    echo "<br>";
+
+
+
+
+
+
+// //CLASS & OBJECT (MATERI 1)
+//     // memanggil class
+//     require "Model/Robot.php";
+//     // membuat objek
+//     $robot1 = new Robot();
+//         // ini adalah cara yang baik untuk mengisi object/nilai dalam property
+
+//         // cara 1
+//         // $robot1->name = "Optimus prime";
+//         // $robot1->color = "blue-white-red";
+//         // $robot1->size = "large";
+
+//         // cara 2
+//         $robot1->set_name("Optimus prime");
+//         $robot1->set_color("blue-white-red");
+//         $robot1->set_size("large");
+
+//     $robot2 = new Robot();
+//         // cara 1
+//         // $robot2->name = "Bumblebee";
+//         // $robot2->color = "yellow";
+//         // $robot2->size = "small";
+//         // cara 2
+//         $robot2->set_name("Bumblebee");
+//         $robot2->set_color("yellow");
+//         $robot2->set_size("small");
+
+//     // melihat objek di halaman
+//     var_dump($robot1);
+//     echo"<br>";
+//     var_dump($robot2);
 ?>
--- model/Robot.php
+++ model/Robot.php
@@ -1,29 +1,63 @@
 <?php
-// CLASS & OBJECT (MATERI 1)
-// Format penggunaan class dibiasakan memiliki nama folder dengan awalan huruf Kapital
-    // membuat class
+    // CONSTRUCTOR & DESTRUCTOR (MATERI 2)
+    // Constructor : Dijalankan ketika objek dibuat
     class Robot {
-        // membuat property
-            // public $name = "Optimus prime";
-            // public $color = "blue-white-red";
-            // public $size = "large";
-        // cara diatas kurang cocok untuk digunakan karena semua object di dalam property memiliki nilai yang sama
+        public $name;
+        public $color;
+        public $size;
 
-            public $name;
-            public $color;
-            public $size;
+        // // [1] Testing 
+        // // function __construct : otomatis file function __construct dijalankan terlebih dahulu ketika objek dibuat
+        // function __construct() {
+        //     echo "fungsi construct sedang dijalankan";
+        // }
+        // function test() {
+        //     echo "ini adalah function test";
+        // }
 
-            //cara 2
-            //fyi : function disini termasuk dalam public. beberapa tipe lain yaitu, public, private, dan protected
-            function set_name($name) {
-                $this->name = $name;
-            }
-            function set_color($color) {
-                $this->color = $color;
-            }
-            function set_size($size) {
-                $this->size = $size;
-            }
+        // Property diisi ke dalam construct, dengan kata lain ketika objek dibuat otomatis property akan terbuat sendiri, sehingga kita tak harus memanggil lagi nama objek di dalam property tersebut berulang kali.
+        function __construct($name, $color, $size) {
+            $this->name = $name;
+            $this->color = $color;
+            $this->size = $size;
+        }
 
+    // Destructor : Dijalankan ketika objek telah selesai dikerjakan semua 
+        function __destruct() {
+            echo "ini adalah isi dari function destruct";
+        }
     }
+
+
+
+
+
+
+// // CLASS & OBJECT (MATERI 1)
+// // Format penggunaan class dibiasakan memiliki nama folder dengan awalan huruf Kapital
+//     // membuat class
+//     class Robot {
+//         // membuat property
+//             // public $name = "Optimus prime";
+//             // public $color = "blue-white-red";
+//             // public $size = "large";
+//         // cara diatas kurang cocok untuk digunakan karena semua object di dalam property memiliki nilai yang sama
+
+//             public $name;
+//             public $color;
+//             public $size;
+
+//             //cara 2
+//             //fyi : function disini termasuk dalam public. beberapa tipe lain yaitu, public, private, dan protected
+//             function set_name($name) {
+//                 $this->name = $name;
+//             }
+//             function set_color($color) {
+//                 $this->color = $color;
+//             }
+//             function set_size($size) {
+//                 $this->size = $size;
+//             }
+//     }
+
 ?>
