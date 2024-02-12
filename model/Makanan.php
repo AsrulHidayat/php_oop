521ce0287649af39d93c6a3c56dc3169e2f18ca5
Asrul Hidayat <136725199+AsrulHidayat@users.noreply.github.com>
2024-02-12 10:53:21 +0800

    MATERI 4 - mengakses private visibility

--- model/Makanan.php
+++ model/Makanan.php
@@ -11,9 +11,26 @@
     // contoh :
     class Makanan {
         public $nama = "Semangka";
+        // [2] mengakses private visibility
+        private $jenis = "Pencuci Mulut";
+            // [2.1] Cara agar bisa diakses
+                //  cara agar bisa diakses dengan menggunakan method yang harus memiliki tipe public : contohnya "public function".
+        public function getJenis(){
+                // return memiliki fungsi untuk mengembalikan nilai dalam hal ini di sini yaitu nilai dari "jenis"
+            return $this->jenis;
+        }
     }
 
     $makanan1 = new Makanan();
     echo $makanan1->nama;
+    // [2] mengakses private visibility
+        // Nah, dibagian ini akan mengalami eror berbeda dengan public code sebelum ditambahkan yang ini. Karena pada dasarnya private itu hanya bisa diakses oleh class tersebut. Kode di bawah sendiri adalah kode yang ada di luar class.
+    echo "<br/>";
+    // [2] mengakses private visibility
+    // echo $makanan1->jenis;
+        // [2.1] Cara agar bisa diakses
+                // Nah, untuk mengaksesnya sendiri agar bisa berjalan kita tambahkan didepannya kata "get" untuk mendapatkan nilai dari properti jenis.
+    echo $makanan1->getJenis();
+
 
     ?>
